d34fac09ba5936b2e0983b8b22e4bbd272158305
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 17:54:18 -0500

    Add form for adding new Recipes

--- app/javascript/components/NewRecipe.jsx
+++ app/javascript/components/NewRecipe.jsx
@@ -49,6 +49,60 @@ const NewRecipe = () => {
             .then((response) => navigate(`/recipe/${response.id}`))
             .catch((error) => console.log(error.message));
     };
+
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-sm-12 col-lg-6 offset-lg-3">
+                    <h1 className="font-weight-normal mb-5">
+                        Add a new recipe to our awesome recipe collection.
+                    </h1>
+                    <form onSubmit={onSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="recipeName">Recipe name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="recipeName"
+                                className="form-control"
+                                required
+                                onChange={(event) => onChange(event, setName)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="recipeIngredients">Ingredients</label>
+                            <input
+                                type="text"
+                                name="ingredients"
+                                id="recipeIngredients"
+                                className="form-control"
+                                required
+                                onChange={(event) => onChange(event, setIngredients)}
+                            />
+                            <small id="ingredientsHelp" className="form-text text-muted">
+                                Separate each ingredient with a comma.
+                            </small>
+                        </div>
+                        <label htmlFor="instruction">Preparation Instructions</label>
+                        <textarea
+                            className="form-control"
+                            id="instruction"
+                            name="instruction"
+                            rows="5"
+                            required
+                            onChange={(event) => onChange(event, setInstruction)}
+                        />
+                        <button type="submit" className="btn custom-button mt-3">
+                            Create Recipe
+                        </button>
+                        <Link to="/recipes" className="btn btn-link mt-3">
+                            Back to recipes
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default NewRecipe;
