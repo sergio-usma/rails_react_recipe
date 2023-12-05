68ea8cc21d09ebb3af6fe48d182614d2f0053b03
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 17:53:26 -0500

    Add onChange & onSubmit events to POST data to rails API

--- app/javascript/components/NewRecipe.jsx
+++ app/javascript/components/NewRecipe.jsx
@@ -13,6 +13,42 @@ const NewRecipe = () => {
             .replace(/</g, "&lt;")
             .replace(/>/g, "&gt;");
     };
+
+    const onChange = (event, setFunction) => {
+        setFunction(event.target.value);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const URL = "/api/v1/recipes/create";
+
+        if (name.length === 0 || ingredients.length === 0 || instruction.length === 0)
+            return;
+
+        const body = {
+            name,
+            ingredients,
+            instruction: stripHtmlEntities(instruction),
+        };
+
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+        fetch(URL, {
+            method: "POST",
+            headers: {
+                "X-CSRF-Token": token,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then((response) => navigate(`/recipe/${response.id}`))
+            .catch((error) => console.log(error.message));
+    };
 };
 
 export default NewRecipe;
