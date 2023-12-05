7177c07ba5225a2fdaa0f328614dd3d3153989cc
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 17:25:46 -0500

    Add Route path for rendering Recipe component

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Recipes";
+import Recipe from "../components/Recipe";
 
 export default (
     <Router>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/recipes" element={<Recipes />} />
+            <Route path="/recipe/:id" element={<Recipe />} />
         </Routes>
     </Router>
 );
