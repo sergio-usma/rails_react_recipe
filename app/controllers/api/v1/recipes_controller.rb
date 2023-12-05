fabb68bbdc3008a40bcec127c0ee3ad53bc7f8f3
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 16:35:16 -0500

    Add index & create methods for recipes_controller using strong params

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,13 +1,27 @@
 class Api::V1::RecipesController < ApplicationController
   def index
+    recipe = Recipe.all.order(created_at: :desc)
+    render json: recipe
   end
 
   def create
+    recipe = Recipe.create!(recipe_params)
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:name, :image, :ingredients, :instruction)
   end
 end
