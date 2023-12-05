648f0bf8109a9072278783bc5c4d309ff83bd543
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 16:30:14 -0500

    Update routes for recipes_controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get 'show/:id', to: 'recipes#show'
+      delete 'destroy/:id', to: 'recipes#destroy'
+    end
+  end
   root 'homepage#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/*path' => 'homepage#index'
 end
