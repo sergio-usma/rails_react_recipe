1c98e149f060a2dceefcedd42ca1fd157a2ea70e
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 17:24:54 -0500

    Add card view for individual recipe details

--- app/javascript/components/Recipe.jsx
+++ app/javascript/components/Recipe.jsx
@@ -22,6 +22,69 @@ const Recipe = () => {
     const addHtmlEntities = (str) => {
         return String(str).replace(/&lt;/g, "<").replace(/&gt;/g, ">");
     };
+
+    const ingredientList = () => {
+        let ingredientList = "No ingredients available";
+
+        if (recipe.ingredients.length > 0) {
+            ingredientList = recipe.ingredients
+                .split(",")
+                .map((ingredient, index) => (
+                    <li key={index} className="list-group-item">
+                        {ingredient}
+                    </li>
+                ));
+        }
+
+        return ingredientList;
+    };
+
+    const recipeInstruction = addHtmlEntities(recipe.instruction);
+
+    return (
+        <div className="">
+            <div className="hero position-relative d-flex align-items-center justify-content-center">
+                <img
+                    src={recipe.image}
+                    alt={`${recipe.name} image`}
+                    className="img-fluid position-absolute"
+                />
+                <div className="overlay bg-dark position-absolute" />
+                <h1 className="display-4 position-relative text-white">
+                    {recipe.name}
+                </h1>
+            </div>
+            <div className="container py-5">
+                <div className="row">
+                    <div className="col-sm-12 col-lg-3">
+                        <ul className="list-group">
+                            <h5 className="mb-2">Ingredients</h5>
+                            {ingredientList()}
+                        </ul>
+                    </div>
+                    <div className="col-sm-12 col-lg-7">
+                        <h5 className="mb-2">Preparation Instructions</h5>
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: `${recipeInstruction}`,
+                            }}
+                        />
+                    </div>
+                    <div className="col-sm-12 col-lg-2">
+                        <button
+                            type="button"
+                            className="btn btn-danger"
+                        >
+                            Delete Recipe
+                        </button>
+                    </div>
+                </div>
+                <Link to="/recipes" className="btn btn-link">
+                    Back to recipes
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Recipe;
