458652c20e2eecb578553293dc3d391a8a9917d3
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 17:47:15 -0500

    Replace image for a colombian 'Bandeja paisa'

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
     name: "Recipe #{i + 1}",
     ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
     instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.',
-    image: 'https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://th.bing.com/th/id/R.6bee8b3b8a1d49225a7985a3ed7f5924?rik=ySYCpSTG9S7oxA&pid=ImgRaw&r=0'
   )
 end
 
