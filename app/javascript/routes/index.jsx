150f5d7886dc82e3d85014c05a2ecf848abbdc59
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 17:55:06 -0500

    Add Route path for NewRecipe component

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Recipes";
 import Recipe from "../components/Recipe";
+import NewRecipe from "../components/NewRecipe";
 
 export default (
     <Router>
@@ -10,6 +11,7 @@ export default (
             <Route path="/" element={<Home />} />
             <Route path="/recipes" element={<Recipes />} />
             <Route path="/recipe/:id" element={<Recipe />} />
+            <Route path="/recipe" element={<NewRecipe />} />
         </Routes>
     </Router>
 );
