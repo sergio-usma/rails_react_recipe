59627e914b91ef56cc3832eb35f14f73c15f862d
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 17:13:14 -0500

    Add images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
   Recipe.create(
     name: "Recipe #{i + 1}",
     ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
-    instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
+    instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.',
+    image: 'https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
   )
 end
 
