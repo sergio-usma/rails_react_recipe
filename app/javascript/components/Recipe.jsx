c1bf5a74d4a07f018600c813986067d47fc4e6d1
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-05 17:22:10 -0500

    Add useEffect to fetch recipe details from rails API

--- app/javascript/components/Recipe.jsx
+++ app/javascript/components/Recipe.jsx
@@ -5,6 +5,19 @@ const Recipe = () => {
     const params = useParams();
     const navigate = useNavigate();
     const [recipe, setRecipe] = useState({ ingredients: "" });
+
+    useEffect(() => {
+        const URL = `/api/v1/show/${params.id}`;
+        fetch(URL)
+            .then((response) => {
+                if(response.ok) {
+                    return response.json();
+                }
+            throw new Error('Network response was not ok.');
+            })
+        .then((response) => setRecipe(response))
+        .catch(() => navigate('/recipes'));
+    },[params.id])
 };
 
 export default Recipe;
